5133dfe0f16fcdeb808c25ea4adae225d7e8744e
alice.lepissier@gmail.com <alice.lepissier@gmail.com>
2021-08-30 02:10:18 +0000

    Create Figures directory in postBuild

--- postBuild.sh
+++ postBuild.sh
@@ -2,5 +2,5 @@ cd ~/work
 wget -q -O ~/work/Data.zip https://github.com/walice/illicitAI-data/raw/main/Data.zip
 unzip ~/work/Data.zip -d ~/work/
 rm ~/requirements.R -f
-mkdir ~/work/Results/
+mkdir ~/work/Results/ ~/work/Figures/ ~/work/Figures/Network_Maps/
 #papermill Scripts/Predictions.ipynb Scripts/Predictions.ipynb
