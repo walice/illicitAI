e53fd6d65e6028c1d0bedf27b4077f39e2b7fc65
alice.lepissier@gmail.com <alice.lepissier@gmail.com>
2021-09-20 06:34:39 +0000

    Park jupyter-server-extensions here

--- extensions.sh
+++ extensions.sh
@@ -2,3 +2,13 @@ git clone git://github.com/minrk/nbextension-scratchpad
 jupyter nbextension install nbextension-scratchpad --sys-prefix
 jupyter nbextension enable nbextension-scratchpad/main --sys-prefix
 rm ~/work/nbextension-scratchpad/ -rf
+
+pip install nbresuse 
+jupyter serverextension enable --py nbresuse --sys-prefix
+jupyter nbextension install --py nbresuse --sys-prefix
+jupyter nbextension enable --py nbresuse --sys-prefix
+
+pip install jupyter-resource-usage
+jupyter serverextension enable --py jupyter-resource-usage --sys-prefix
+jupyter nbextension install --py jupyter-resource-usage --sys-prefix
+jupyter nbextension enable --py jupyter-resource-usage --sys-prefix
