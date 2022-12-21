b4ab66c12c2a6c005028660a2967cebfdf081aa8
Alice Lepissier <alice.lepissier@gmail.com>
2022-12-21 22:51:04 +0000

    Extract zip to proper Data folder

--- postBuild.sh
+++ postBuild.sh
@@ -2,7 +2,7 @@ cd ~/work
 wget -q -O ~/work/Data.zip https://zenodo.org/record/7470475/files/Data.zip?download=1
 mkdir ~/work/Data
 cd ~/work/Data
-unzip ~/work/Data.zip -d ~/work/
+unzip ~/work/Data.zip -d ~/work/Data
 rm ~/requirements.R -f
 mkdir ~/work/Results/ ~/work/Figures/ ~/work/Figures/Network_Maps/
 #papermill Scripts/Predictions.ipynb Scripts/Predictions.ipynb
