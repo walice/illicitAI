56157a5087e675893d674a702c5daee2c8285283
Alice Lepissier <alice.lepissier@gmail.com>
2022-12-21 22:13:27 +0000

    Download Data folder from Zenodo

--- postBuild.sh
+++ postBuild.sh
@@ -1,5 +1,7 @@
 cd ~/work
-wget -q -O ~/work/Data.zip https://github.com/walice/illicitAI-data/raw/main/Data.zip
+wget -q -O ~/work/Data.zip https://zenodo.org/record/7470475/files/Data.zip?download=1
+mkdir ~/work/Data
+cd ~/work/Data
 unzip ~/work/Data.zip -d ~/work/
 rm ~/requirements.R -f
 mkdir ~/work/Results/ ~/work/Figures/ ~/work/Figures/Network_Maps/
